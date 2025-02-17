df139babc6b259eb2e8d417cb1668bf809b1414c
vonssy <kak.reyvaldizakaria@gmail.com>
2025-02-17 15:36:05 +0700

    Fix Bug in Load Auto Proxies

--- bot.py
+++ bot.py
@@ -53,7 +53,7 @@ class NodeGo:
             if use_proxy_choice == 1:
                 response = await asyncio.to_thread(requests.get, "https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/all.txt")
                 response.raise_for_status()
-                content = response.text()
+                content = response.text
                 with open(filename, 'w') as f:
                     f.write(content)
                 self.proxies = content.splitlines()
